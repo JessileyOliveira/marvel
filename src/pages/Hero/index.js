f750b38418704e0d97f188be0c834f160f8ad173
Jessiley <xjaco222x@hotmail.com>
2020-05-10 15:29:21 -0300

    Feat: Added complete hero componet

--- src/pages/Hero/index.js
+++ src/pages/Hero/index.js
@@ -1,7 +1,125 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 
-import { Container } from './styles';
+import api from '../../services/marvelApi';
+import params from '../../services/marvelParams';
+import Loader from '../../components/Loader';
+
+import { Container, ImageHero, DescriptionConatiner, More } from './styles';
 
 export default function Hero() {
-  return <Container />;
+  const { heroId } = useParams();
+  const history = useHistory();
+  const [endList, setEndList] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [loadingSerie, setLoadingSerie] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hero, setHero] = useState({});
+  const [isMounted, setIsMounted] = useState(false);
+  const getHero = async () => {
+    setLoading(true);
+    const apiParans = {
+      ts: params.ts,
+      apikey: params.apikey,
+      hash: params.hash,
+    };
+    const response = await api.get(`/characters/${heroId}`, {
+      params: apiParans,
+    });
+    setHero(response.data.data.results[0]);
+    if (
+      response.data.data.results[0].series.available <=
+      response.data.data.results[0].series.returned
+    ) {
+      setEndList(true);
+    }
+
+    setLoading(false);
+  };
+
+  const getSeries = async () => {
+    setLoadingSerie(true);
+    const limit = 20;
+    const apiParans = {
+      ts: params.ts,
+      apikey: params.apikey,
+      hash: params.hash,
+      limit,
+      offset: page === 1 ? 0 : (page - 1) * limit,
+    };
+    const response = await api.get(`/characters/${heroId}/series`, {
+      params: apiParans,
+    });
+
+    if (response.data.data.count < response.data.data.limit) setEndList(true);
+    const series = hero.series.items;
+    response.data.data.results.forEach((serie) => {
+      series.push({
+        name: serie.title,
+        resourceURI: serie.resourceURI,
+      });
+    });
+    const heroWithNewSeries = hero;
+    heroWithNewSeries.series.items = series;
+    setHero(heroWithNewSeries);
+
+    setLoadingSerie(false);
+  };
+
+  useEffect(() => {
+    getHero();
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) getSeries();
+    setIsMounted(true);
+  }, [page]);
+
+  const handleVoltar = () => {
+    history.goBack();
+  };
+  return (
+    <Container>
+      {hero.id && (
+        <>
+          <ImageHero
+            data-testid="heroImg"
+            src={`${hero.thumbnail.path}/portrait_uncanny.${hero.thumbnail.extension}`}
+            alt={hero.name}
+          />
+          <DescriptionConatiner>
+            <div id="name">
+              <h2>{hero.name}</h2>
+              {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+              <span onClick={handleVoltar} role="button" tabIndex={0}>
+                Voltar
+              </span>
+            </div>
+            <h3>Series:</h3>
+            <ul data-testid="seriesList">
+              {hero.series.items.map((serie) => (
+                <li key={serie.name}>{serie.name}</li>
+              ))}
+            </ul>
+            {loadingSerie && <Loader width={28} border={4} />}
+            {!loadingSerie &&
+              !loading &&
+              (endList ? (
+                <More end="true">Sem mais Series!</More>
+              ) : (
+                <More
+                  onClick={() => {
+                    setLoadingSerie(true);
+                    setPage(page + 1);
+                  }}
+                >
+                  Carregar mais
+                </More>
+              ))}
+          </DescriptionConatiner>
+        </>
+      )}
+      {loading && <Loader />}
+    </Container>
+  );
 }
--- src/pages/Hero/styles.js
+++ src/pages/Hero/styles.js
@@ -3,4 +3,74 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   background: #fff;
+  margin: 12px;
+  padding: 12px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  -webkit-box-shadow: -2px 4px 5px 0px rgba(50, 50, 50, 0.41);
+  -moz-box-shadow: -2px 4px 5px 0px rgba(50, 50, 50, 0.41);
+  box-shadow: -2px 4px 5px 0px rgba(50, 50, 50, 0.41);
+`;
+
+export const ImageHero = styled.img`
+  border-radius: 4px;
+  object-fit: cover;
+  max-height: 450px;
+`;
+
+export const DescriptionConatiner = styled.div`
+  width: 100%;
+  padding: 0px 8px;
+  display: flex;
+  flex-direction: column;
+  & > #name {
+    width: 100%;
+    border-bottom: 1px solid #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+
+  & > #name > span {
+    color: #e71c22;
+    cursor: pointer;
+    transition: color 0.4s;
+  }
+
+  & > #name > span:hover {
+    color: #000;
+  }
+
+  & > h3 {
+    width: 100%;
+    margin-top: 8px;
+  }
+
+  & > ul {
+    width: 100%;
+    margin-left: 8px;
+    list-style-type: '- ';
+  }
+
+  & > ul > li {
+    padding: 4px;
+    border-bottom: 1px solid #eee;
+  }
+`;
+
+export const More = styled.div`
+  width: 100%;
+  text-align: center;
+  font-size: 14px;
+  font-weight: bold;
+  color: #aaa;
+  cursor: ${(props) => (props.end ? 'default' : 'pointer')};
+  padding: 8px;
+  border-bottom: 1px solid #eee;
+  transition: color 0.4s;
+
+  &:hover {
+    color: ${(props) => (props.end ? '#aaa' : '#e71d23')};
+  }
 `;
