41376242d38f6508a7bd445c1cc49f11a5daea27
Jessiley <xjaco222x@hotmail.com>
2020-05-10 14:37:52 -0300

    Refactor: Added width and border props

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Loading } from './styles';
 
-function Loader() {
+function Loader(props) {
+  const { width, border } = props;
   return (
     <Container>
-      <Loading />
+      <Loading width={width} border={border} />
     </Container>
   );
 }
+
+Loader.propTypes = {
+  width: PropTypes.number,
+  border: PropTypes.number,
+};
+
+Loader.defaultProps = {
+  width: 60,
+  border: 8,
+};
 
 export default Loader;
